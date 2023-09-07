3d47953fb332aa1114b4de8bd7b21049288913ab
Honrichie <offeirichard130@gmail.com>
2023-09-07 12:12:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that helps you generate a line using puts function
+ * Return:0 (Correct)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
