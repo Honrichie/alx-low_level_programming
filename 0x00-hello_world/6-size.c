fff8b12568433b485e6854da5e37f3953afd8fba
Honrichie <offeirichard130@gmail.com>
2023-09-09 15:07:17 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
  * main - A program that print the size of various computer types
  * Return 0 (sucess)
  */
-int main(void)
+int main (void)
 {
 printf("Size of a char: %lu  byte(s)\n", sizeof(char);
 printf("Size of an int: %lu  byte(s)\n", sizeof(int);
