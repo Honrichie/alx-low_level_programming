1e819dd09c251a5b709d8f2ef7a9723d9609a671
Honrichie <offeirichard130@gmail.com>
2023-09-11 14:43:57 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 	char i;
-	for (i = 'a' ; i <= 'z' ; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		if (i != 'q' && i != 'e')
 		putchar(i);
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }       
