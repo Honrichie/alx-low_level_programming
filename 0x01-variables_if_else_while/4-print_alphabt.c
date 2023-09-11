d2933daebddd3c6d09be235dcb4e1ffc22dd0da5
Honrichie <offeirichard130@gmail.com>
2023-09-11 12:38:39 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,16 +3,15 @@
 /**
  * main - main function
  *
- * Return: 0 (sucess)
+ * Return: 0(sucess)
  */
-
 int main(void)
 {
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
 		if (i != 'q' && i != 'e')
-	putchar(i);
+		putchar(i);
 	putchar('\n');
 	return (0);
 }       
