10e8d38f8de39f8281bf43a8fea044f47a96383c
Honrichie <offeirichard130@gmail.com>
2023-09-13 08:38:15 -0700

    #include <stdio.h>
    /**
     * main - main function
     *
     * Return: 0 (success)
     */
    
    int main(void)
    {
            int i;
    
            for (i = 0 ; i < 10 ; i++)
                    putchar(i + '0');
            putchar('\n');
            return (0);
    }
    cd ..
    
    clear

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - main function
  *
- * Return: 0 (sucess)
+ * Return: 0 (success)
  */
 
 int main(void)
 {
-	int i;
-	
-	for (i = 0; i < 10; i++)
-		putchar("%d", i);
-	putchar("\n");
-	return (0);
+        int i;
+
+        for (i = 0 ; i < 10 ; i++)
+                putchar(i + '0');
+        putchar('\n');
+        return (0);
 }
