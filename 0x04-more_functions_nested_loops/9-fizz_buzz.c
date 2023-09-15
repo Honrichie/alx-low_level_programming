80c3890940cfb8b3baa01e3f803cec904501e648
Honrichie <offeirichard130@gmail.com>
2023-09-15 01:48:13 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 
 /**
  * main - func prints the numbers from 1-100, but for multiplication of three,
- * Fizz is printed instead of the number, for multiples of five,
- * Buzz, and for multiples of both three and five, FizzBuzz
+ *	Fizz is printed instead of the number, for multiples of five,
+ *	Buzz, and for multiples of both three and five, FizzBuzz
  *
  * Return: Always 0
  */
@@ -30,7 +30,7 @@ int main(void)
 		printf(" ");
 	}
 
-	printf('\n');
+	printf("\n");
 
 	return (0);
 }
